e78d3e9b794e510ce70a1923e544cb7ee8a24d70
rohit goudagaon <rohitsgoudagaon@synerzip.com>
2019-11-21 16:53:07 +0530

    minor change in lint script

--- src/modules/user/component/userComponent.js
+++ src/modules/user/component/userComponent.js
@@ -11,7 +11,7 @@ export const UserComponent = props => {
       <br />
       <br />
       <Button variant="contained" color="secondary" onClick={updateUserName}>
-        Set User name
+        Change User name
       </Button>
     </React.Fragment>
   );
